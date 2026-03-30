a812698676ef2ed6dd835d156aa0fd1f591b3325
linus <linusedstrom80@gmail.com>
2026-03-30 09:40:15 +0200

    lägger upp main

---
+++ src/test/java/com/edstrom/WigellMcRental/WigellMcRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edstrom.WigellMcRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WigellMcRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
